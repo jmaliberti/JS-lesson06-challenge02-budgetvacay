2d85c9d61fcc985da87552e95c499044ad0c0795
Jessica Aliberti <jmaliberti@gmail.com>
2024-03-15 13:20:59 -0700

    Added calls for variables to tripinfo function

--- js/script.js
+++ js/script.js
@@ -3,6 +3,15 @@ var dailyBudget = document.querySelector("daily-budget");
 
 var tripInfo = function () {
     var totalBudget = Number(prompt(`What is your total budget?`))
-    var accomodation = Number(prompt(`What are your accommodation costs?`));
+    var accommodation = Number(prompt(`What are your accommodation costs?`));
     var numDays = Number(prompt(`How many days does your trip last?`));
+};
+
+totalBudget();
+accommodation();
+numDays();
+
+var calculateDailyBudget = function (totalBudget,accommodation,numDays) {
+    var daily = (totalBudget-accommodation)/numDays;
+    dailyBudget.innerText = "You can spend $${daily} a day on food and fun";
 };
