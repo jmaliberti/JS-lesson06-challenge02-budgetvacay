338b0178764e540790cf95e883ad2cfb094628f5
Jessica Aliberti <jmaliberti@gmail.com>
2024-03-15 12:59:07 -0700

    created all variables

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var tripInfoButton = document.querySelector("tip-info");
+var dailyBudget = document.querySelector("daily-budget");
+
+var tripInfo = function () {
+    var totalBudget = Number(prompt(`What is your total budget?`))
+    var accomodation = Number(prompt(`What are your accommodation costs?`));
+    var numDays = Number(prompt(`How many days does your trip last?`));
+};
